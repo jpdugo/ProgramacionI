46940ee44dfdf3bd74edf50acd2a55ea38800ae0
Juan Patricio Dugo <98497626+jpdugo@users.noreply.github.com>
2024-09-14 23:15:54 -0300

    Refactor code to use dynamic memory allocation for vector in main.c

--- clase_4/main.c
+++ clase_4/main.c
@@ -7,20 +7,23 @@
 #define TOPE 10
 
 // function protoypes
-int *completar_vector(int largo);
-void imprimir_vector(int vector[], int largo);
+int *completar_vector(int *largo);
+void imprimir_vector(int vector[], int *largo);
 
 int main()
 {
-  int *vector = completar_vector(TOPE);
-  printf("Vector sin ordenar: ");
-  imprimir_vector(vector, TOPE);
+  int tope_value = TOPE;
+  int *tope = &tope_value;
 
-  int intervalo = TOPE / 2;
+  int *vector = completar_vector(tope);
+  printf("Vector sin ordenar: ");
+  imprimir_vector(vector, tope);
+
+  int intervalo = *tope / 2;
   while (intervalo > 0)
   {
 
-    for (int i = intervalo; i < TOPE; i++)
+    for (int i = intervalo; i < *tope; i++)
     {
       int j = i - intervalo;
       while (j >= 0)
@@ -43,20 +46,20 @@ int main()
 
     intervalo = intervalo / 2;
   }
-  
+
   printf("Vector ordenado: ");
-  imprimir_vector(vector, TOPE);
+  imprimir_vector(vector, tope);
 
   return 0;
 }
 
-int *completar_vector(int largo)
+int *completar_vector(int *largo)
 {
-  int * vector = (int *)malloc(largo * sizeof(int));
+  int *vector = (int *)malloc(*largo * sizeof(int));
 
   srand(time(NULL));
 
-  for (int i = 0; i < largo; i++)
+  for (int i = 0; i < *largo; i++)
   {
     vector[i] = rand() % 101;
   }
@@ -64,12 +67,12 @@ int *completar_vector(int largo)
   return vector;
 }
 
-void imprimir_vector(int vector[], int largo)
+void imprimir_vector(int vector[], int *largo)
 {
-  for (int i = 0; i < largo - 1; i++)
+  for (int i = 0; i < *largo - 1; i++)
   {
     printf("%d - ", vector[i]);
   }
 
-  printf("%d\n", vector[largo - 1]);
+  printf("%d\n", vector[*largo - 1]);
 }
