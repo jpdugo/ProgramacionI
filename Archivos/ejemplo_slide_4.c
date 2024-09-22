e214f23d678bef03de112127929edbb26a683649
Juan Patricio Dugo <98497626+jpdugo@users.noreply.github.com>
2024-09-22 16:34:09 -0300

    Add gitignore rules for tempCodeRunnerFile and *.txt files

--- Archivos/ejemplo_slide_4.c
+++ Archivos/ejemplo_slide_4.c
@@ -1,32 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX 20
 
 int main()
 {
-  FILE *archivo = NULL;
-  char texto[50];
+  FILE *archivo_enteros = NULL;
+  FILE *archivo_resultados = NULL;
+  char texto[MAX];
+  int contador_pares = 0;
 
-  if((archivo = fopen("enteros.txt", "w")) == NULL) {
+  if ((archivo_enteros = fopen("enteros.txt", "w+")) == NULL)
+  {
     printf("Error al abrir el archivo\n");
     return -1;
   }
 
-  printf("%d \n", ferror(archivo));
+  printf("%d \n", ferror(archivo_enteros));
 
-  printf("Ingrese una cadena de texto:\n");
+  printf("Ingrese una secuencia de numeros enteros:\n");
   fflush(stdin);
-
   scanf(" %[^\n]", texto);
+  fprintf(archivo_enteros, "%s\n", texto);
 
-  fprintf(archivo, "%s\n", texto);
-
-  if (ferror(archivo)) {
+  if (ferror(archivo_enteros))
+  {
     perror("Error occurred");
   }
 
-  fclose(archivo);
+  rewind(archivo_enteros);
 
-  printf("Fin del archivo!");
+  int i = 0;
+  int value;
+  archivo_resultados = fopen("resultados.txt", "w+");
+  while (fscanf(archivo_enteros, "%1d", &value) == 1 && i < MAX)
+  {
+    
+    if (value % 2 == 0)
+    {
+      fprintf(archivo_resultados, "%d", value);
+      contador_pares++;
+    }
+    i++;
+  }
+
+  rewind(archivo_resultados);
+  
+  char resultados[contador_pares];
+  fgets(resultados, contador_pares + 1, archivo_resultados);
+  
+  printf("Hay %d numeros pares: %s", contador_pares, resultados);
+
+  fclose(archivo_enteros);
+  fclose(archivo_resultados);
+
+  printf("\nFin del programa!");
 
   return 0;
 }
