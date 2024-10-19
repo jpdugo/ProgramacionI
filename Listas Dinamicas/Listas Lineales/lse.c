b7d95fd6ab9b5c73017d22128d021bf070e24c95
Juan Patricio Dugo <98497626+jpdugo@users.noreply.github.com>
2024-10-19 18:48:34 -0300

    comienzo con listas simples enlazadas

---
+++ Listas Dinamicas/Listas Lineales/lse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct 
+{
+    char nombre;
+    int valor;
+} Dato;
+
+typedef struct s 
+{
+    Dato elemento;
+    struct s *siguiente;
+} ElementoLista;
+
+void error();
+
+int main()
+{
+
+}
+
+void error()
+{
+    printf("Insuficiente memoria\n");
+    exit(1);
+}
